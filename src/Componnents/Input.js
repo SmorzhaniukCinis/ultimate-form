c5cea49ed1b0a94858400c465398a3ecdac8925c
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-02-02 18:59:53 +0200

    step 1 complete
    end write first step of form

--- src/Componnents/Input.js
+++ src/Componnents/Input.js
@@ -4,8 +4,6 @@ import {makeStyles, TextField} from "@material-ui/core";
 
 
 export const Input = forwardRef((props, ref) => {
-
-
     return (
         <TextField variant={'outlined'} margin={'normal'} inputRef={ref} fullWidth {...props}/>
     )
--- src/Componnents/Step1.js
+++ src/Componnents/Step1.js
@@ -1,31 +1,58 @@
 import React from "react";
-import {Typography} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import {TextField, Typography} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 import {MainContainer} from "./MainContainer";
 import {Form} from "./Form";
 import {Input} from "./Input";
-
-const useStyles = makeStyles((theme=>({
-    root: {
-        fontFamily: 'Permanent Marker',
-        margin: theme.spacing(3, 0, 2),
-        textAlign: 'center',
-        fontSize: '40px',
-        color: 'deeppink',
-        textShadow: '1px 1px darkmagenta'
-    }
-})))
+import {useForm} from "react-hook-form";
+import {PrimaryButton} from "./PrimaryButton";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from 'yup';
 
 export const Step1 = () => {
-    const styles = useStyles()
-  return(
-     <MainContainer>
-         <Typography component={'h2'} variant={'h5'}>
-             Step 1
-         </Typography>
-         <Form>
-            <Input/>
-         </Form>
-     </MainContainer>
-  )
+
+    const schema = yup.object().shape({
+        firstName: yup
+            .string()
+            .matches(/^([^0-9]*)$/, 'First name should not contain numbers')
+            .required('First name is a required field'),
+        lastName: yup
+            .string()
+            .matches(/^([^0-9]*)$/, 'Last name should not contain numbers')
+            .required('Last name is a required field')
+    })
+
+    const {register, handleSubmit, formState} = useForm({
+        mode: "onBlur",
+        resolver: yupResolver(schema)
+    })
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <MainContainer>
+            <Typography component={'h2'} variant={'h5'}>
+                Step 1
+            </Typography>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    helperText={errors?.firstName?.message}
+                    error={!!errors?.firstName}
+                    {...register("firstName")}
+                    id='firstName'
+                    type='text'
+                    label='First Name'/>
+                <Input
+                    helperText={errors?.lastName?.message}
+                    error={!!errors?.lastName}
+                    {...register("lastName")}
+                    id='lastName'
+                    type='text'
+                    label='Last Name' name='lastName'/>
+                <PrimaryButton type={'submit'}>Next</PrimaryButton>
+            </Form>
+        </MainContainer>
+    )
 }
